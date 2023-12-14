1b8565a666d94aa80d7f55c132814ff154b13f96
JefGoos <jef.goos@ugent.be>
2023-12-14 10:42:54 +0100

    created a new folder 'price'

---
+++ components/specificComponents/Price/Price.js
@@ -0,0 +1,13 @@
+3.	import React, { Component } from "react";
+4.	
+5.	export default class Price extends Component {
+6.	    constructor(props) {
+7.	        super(props);
+8.	    }
+9.	    render() {
+10.	        return (
+11.	            <div>
+12.	                Price:{this.props.blok.title}</div>
+13.	        );
+14.	    }
+15.	}
