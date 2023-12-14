4fd3970fe184f13a8b9151e5a2bbb0c0a11021b7
JefGoos <jef.goos@ugent.be>
2023-12-14 11:02:35 +0100

    code Price

--- components/specificComponents/Price/Price.js
+++ components/specificComponents/Price/Price.js
@@ -1,13 +1,15 @@
-3.	import React, { Component } from "react";
-4.	
-5.	export default class Price extends Component {
-6.	    constructor(props) {
-7.	        super(props);
-8.	    }
-9.	    render() {
-10.	        return (
-11.	            <div>
-12.	                Price:{this.props.blok.title}</div>
-13.	        );
-14.	    }
-15.	}
+
+
+import React, { Component } from "react";
+
+export default class Price extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                Price:{this.props.blok.title}</div>
+        );
+    }
+    15.	}
