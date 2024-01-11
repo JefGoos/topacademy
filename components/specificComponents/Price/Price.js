e80116138e3f11f978c38143a13422940d23b3cd
JefGoos <jef.goos@ugent.be>
2024-01-11 15:57:44 +0100

    price

--- components/specificComponents/Price/Price.js
+++ components/specificComponents/Price/Price.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import css from "./Location.module.scss";
+import css from "./Price.module.scss";
 import Headermenu from "../../genericComponents/Headermenu/Headermenu";
 import Hero from "../../genericComponents/Hero/Hero";
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 import { RichTextToHTML } from "../../../functions/storyBlokRichTextRenderer";
 import List from "../../genericComponents/List/List";
 
-export default class Location extends Component {
+export default class Price extends Component {
 
 	constructor(props) {
 		super(props);
@@ -19,10 +19,10 @@ export default class Location extends Component {
 
 				<main>
 					<Hero blok={this.props.blok} contentTypeTag="course" />
-					<div className={css["location-page__main-content"]}>
-						<div id="location-page__short-description" key="location-page__short-description" className={css["location-page__short-description"]}>
+					<div className={css["price-page__main-content"]}>
+						<div id="price-page__short-description" key="price-page__short-description" className={css["price-page__short-description"]}>
 							<section className={css["rich-text-section--with-navigator"]}>
-								<h2 className={css["rich-text-section__title"]}>Location Details</h2>
+								<h2 className={css["rich-text-section__title"]}>Price Details</h2>
 								<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.description })}</div>
 							</section>
 						</div>
